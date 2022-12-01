2b85d665c5562084cd4c1135958d872ee7a1f5c2
Eemil Majuri <emajuri@c1r5p11.hive.fi>
2022-12-01 18:10:48 +0200

    day 1 part 1

---
+++ day1_1/testmain.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testmain.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emajuri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 13:17:37 by emajuri           #+#    #+#             */
+/*   Updated: 2022/12/01 15:51:53 by emajuri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	int fd;
+	char	*line;
+	int count = 0;
+	int highest = 0;
+
+	argc += 0;
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return (0);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		if (*line != '\n')
+			count += ft_atoi(line);
+		else
+		{
+			if (highest < count)
+				highest = count;
+			count = 0;
+		}
+		free(line);
+	}
+	close(fd);
+	printf("\n\n\n\n");
+	ft_putnbr_fd(highest, 1);
+	return (0);
+}
