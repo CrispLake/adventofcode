6ae7332e876adbd2147b8372e2fabd7470f2f98b
Eemil Majuri <emajuri@c1r5p11.hive.fi>
2022-12-02 13:30:46 +0200

    1st day done

--- day1_1/testmain.c
+++ day1_1/testmain.c
@@ -6,7 +6,7 @@
 /*   By: emajuri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:17:37 by emajuri           #+#    #+#             */
-/*   Updated: 2022/12/01 15:51:53 by emajuri          ###   ########.fr       */
+/*   Updated: 2022/12/02 13:30:01 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	main(int argc, char **argv)
 	char	*line;
 	int count = 0;
 	int highest = 0;
+	int	second = 0;
+	int third = 0;
 
 	argc += 0;
 	fd = open(argv[1], O_RDONLY);
@@ -33,13 +35,27 @@ int	main(int argc, char **argv)
 		else
 		{
 			if (highest < count)
+			{
+				third = second;
+				second = highest;
 				highest = count;
+			}
+			else if (second < count)
+			{
+				third = second;
+				second = count;
+			}
+			else if (third < count)
+				third = count;
 			count = 0;
 		}
 		free(line);
 	}
 	close(fd);
-	printf("\n\n\n\n");
+	printf("\n\n");
+	ft_putnbr_fd(highest, 1);
+	printf("\n\n");
+	highest += second + third;
 	ft_putnbr_fd(highest, 1);
 	return (0);
 }
