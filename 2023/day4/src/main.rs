57c9fa169d7d0de1de2417bdaa31f72b87fd1f08
CrispLake <eemilmajuri@gmail.com>
2023-12-18 11:50:04 +0200

    d4 p2

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -24,27 +24,26 @@ fn get_nums(slice: &str) -> Vec<String> {
     vec
 }
 
-fn main() -> io::Result<()> {
-    let lines: Vec<String> = lines_from_file("./input");
+fn part_one(lines: &Vec<String>) {
     let mut total = 0;
     for line in lines {
         let mut card_total = 0;
-        println!("{}", line);
+        // println!("{}", line);
         let colon;
         let pipe;
         match line.find(':') {
             Some(index) => colon = index + 2,
-            None => return Ok(()),
+            None => return (),
         }
         match line.find('|') {
             Some(index) => pipe = index + 2,
-            None => return Ok(()),
+            None => return (),
         }
         let win_nums: Vec<String> = get_nums(&line[colon..pipe - 2]);
         let my_nums: Vec<String> = get_nums(&line[pipe..]);
         for num in my_nums {
             if win_nums.contains(&num) {
-                println!("contains: {}", num);
+                // println!("contains: {}", num);
                 card_total *= 2;
                 if card_total == 0 {
                     card_total = 1;
@@ -54,5 +53,56 @@ fn main() -> io::Result<()> {
         total += card_total;
     }
     println!("{}", total);
+}
+
+fn part_two(lines: &mut Vec<String>) {
+    let mut card_counts: Vec<i32> = vec![];
+    for _ in 0..lines.len() {
+        card_counts.push(1);
+    }
+    let mut current_card = 0;
+    for line in lines {
+        let mut card_total = 0;
+        // println!("{}", line);
+        let colon;
+        let pipe;
+        match line.find(':') {
+            Some(index) => colon = index + 2,
+            None => return (),
+        }
+        match line.find('|') {
+            Some(index) => pipe = index + 2,
+            None => return (),
+        }
+        let win_nums: Vec<String> = get_nums(&line[colon..pipe - 2]);
+        let my_nums: Vec<String> = get_nums(&line[pipe..]);
+        for num in my_nums {
+            if win_nums.contains(&num) {
+                card_total += 1;
+            }
+        }
+        let mut tmp;
+        println!("matches: {}", card_total);
+        tmp = current_card + 1;
+        while card_total != 0 {
+            card_counts[tmp] += card_counts[current_card];
+            tmp += 1;
+            card_total -= 1;
+        }
+        card_total = 0;
+        current_card += 1;
+    }
+    let mut total: i32 = 0;
+    for num in card_counts {
+        println!("num: {}", num);
+        total += num;
+    }
+    println!("{}", total);
+}
+
+fn main() -> io::Result<()> {
+    let mut lines: Vec<String> = lines_from_file("./input");
+    part_one(&lines);
+    part_two(&mut lines);
     Ok(())
 }
